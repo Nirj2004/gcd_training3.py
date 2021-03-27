753730aaf1997a399c19099d8f28421f554f13b5
Nirj2004 <75828293+Nirj2004@users.noreply.github.com>
2021-03-27 12:44:13 +0530

    Add files via upload

---
+++ gcd training3.py
@@ -0,0 +1,118 @@
+import os
+import math
+
+#This is a comment
+print("Hello World")
+print(math.gcd(5,10))
+'''
+This is a multi line comment
+
+'''
+#This is also a comment
+print(5+8)
+# if(age<18):
+#     print("hello")
+
+
+#a = 34
+#b = "Nirjhar"
+#c = 43.11
+#print(a + c)
+#print(a - c)
+#print(a * c)
+#print(a / c)
+
+# Wrong syntax
+# nirjhar project = 25
+# 1. variable should only start with a letter or an underscore
+# 2. variable cannot start with a number
+# 3. It can only contain alpha numeric characters
+# 4. VAriable names are case sensitive. Nirjhar and nirjhar are two different variables
+
+
+
+name = "Nirjhar"
+print(name[2:4])
+print (name.strip())
+var = name.lower()
+var = name.upper()
+var = name.replace("ar", "t")
+var = name.replace(", ", '\n')
+print(var)
+
+stri = "this is a "
+name = "joke"
+stri2 = "this is not a "
+print(stri + name)
+print(stri2 + name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
